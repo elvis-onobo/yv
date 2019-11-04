8456062dfc471d79776159bdc3b841a647e1389e
Elvis Onobo <elvis.onobo@gmail.com>
2019-11-04 05:04:10 -0800

    created controller, models and migrations for fund, profile, account, kin

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet" />
+
+
+
+    <!-- Icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
+    <link rel="shortcut icon" href="ico/favicon.png" />
+
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                        <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Funding Categories <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="">
+                                        Transportation
+                                    </a>
+                                    <a class="dropdown-item" href="">
+                                        Agriculture
+                                    </a>
+                                    <a class="dropdown-item" href="">
+                                        Food Production
+                                    </a>
+
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="">
+                                    Dashboard
+                                </a>
+                                <a class="dropdown-item" href="">
+                                    Profile
+                                </a>
+                                <a class="dropdown-item" href="">
+                                    Update Password
+                                </a>
+                                <a class="dropdown-item" href="">
+                                    Account Details
+                                </a>
+                                <a class="dropdown-item" href="">
+                                    Next of Kin
+                                </a>
+                                <!-- <a class="dropdown-item" href="">
+                                    Apply
+                                </a> -->
+
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,434 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>YouVest | Invest In Young Growing Startups</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Your page description here" />
+  <meta name="author" content="" />
+
+  <!-- css -->
+  <link href="css/bootstrap.css" rel="stylesheet" />
+  <link href="css/bootstrap-responsive.css" rel="stylesheet" />
+  <link href="css/prettyPhoto.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/home.css" rel="stylesheet">
+
+  <!-- Theme skin -->
+  <link id="t-colors" href="color/default.css" rel="stylesheet" />
+
+  <!-- Fav and touch icons -->
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
+  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
+  <link rel="shortcut icon" href="ico/favicon.png" />
+
+</head>
+
+<body>
+  <div id="wrapper">
+    <!-- start header -->
+    <header>
+      <div class="top">
+        <div class="container">
+          <div class="row">
+            <div class="span6">
+              <ul class="topmenu">
+                <li><a href="#">Home</a> &#47;</li>
+                <li><a href="#">Terms</a> &#47;</li>
+                <li><a href="#">Privacy policy</a></li>
+              </ul>
+            </div>
+            <div class="span6">
+
+              <ul class="social-network">
+                <li><a href="#" data-placement="bottom" title="Facebook"><i class="icon-circled icon-bglight icon-facebook"></i></a></li>
+                <li><a href="#" data-placement="bottom" title="Twitter"><i class="icon-circled icon-bglight icon-twitter"></i></a></li>
+                <li><a href="#" data-placement="bottom" title="Instagram"><i class="icon-circled icon-instagram icon-bglight"></i></a></li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+
+
+        <div class="row nomargin">
+          <div class="span4">
+            <div class="logo">
+              <h1><a href="index.html"> Youvest</a></h1>
+            </div>
+          </div>
+          <div class="span8">
+            <div class="navbar navbar-static-top">
+              <div class="navigation">
+                <nav>
+                  <ul class="nav topnav">
+                    <li class="active">
+                      <a href="index.html">Home</a>
+                    </li>
+                    <li class="dropdown">
+                      <a href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li class="dropdown">
+                      <a href="#">Register</a>
+                    </li>
+                    <!-- <li>
+                      <a href="contact.html">Apply (Businesses)</a>
+                    </li> -->
+                  </ul>
+                </nav>
+              </div>
+              <!-- end navigation -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- end header -->
+
+    <!-- section intro -->
+    <section id="intro">
+      <div class="intro-content">
+        <h2>Welcome to Youvest!</h2>
+        <h3>Grow Your Money Funding Growing Businesses</h3>
+        <div>
+          <a href="#content" class="btn-get-started scrollto">Get Started</a>
+        </div>
+      </div>
+    </section>
+    <!-- /section intro -->
+
+    <section id="content">
+      <div class="container">
+
+
+        <div class="row">
+          <div class="span12">
+            <div class="row">
+              <div class="span4">
+                <div class="box aligncenter">
+                  <div class="icon">
+                    <span class="badge badge-info badge-circled">1</span>
+                    <i class="ico icon-user icon-5x"></i>
+                  </div>
+                  <div class="text">
+                    <h4><strong>Create Account</strong></h4>
+                    <p>
+                        Sign up and setup your account
+                        quickly with very limited data
+                        requirements.
+                    </p>
+                    <a href="#">Start</a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="span4">
+                <div class="box aligncenter">
+                  <div class="icon">
+                    <span class="badge badge-success badge-circled">2</span>
+                    <i class="ico icon-money icon-5x"></i>
+                  </div>
+                  <div class="text">
+                    <h4><strong>Fund Project</strong></h4>
+                    <p>
+                        Little drops form the sea; start investing and earning
+                         with as little as N1,000
+                    </p>
+                    <a href="#">Learn More</a>
+                  </div>
+                </div>
+              </div>
+              <div class="span4">
+                <div class="box aligncenter">
+                  <div class="icon">
+                    <span class="badge badge-warning badge-circled">3</span>
+                    <i class="ico icon-arrow-up icon-5x"></i>
+                  </div>
+                  <div class="text">
+                    <h4><strong>Get Returns</strong></h4>
+                    <p>
+                        Enjoy consistent returns on your Investment
+                         and start growing your money bag
+                    </p>
+                    <a href="#">Learn More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="row">
+          <div class="span12">
+            <div class="cta-box">
+              <div class="row">
+                <div class="span8">
+                  <div class="cta-text">
+                    <h2>Earn Up To 15% Returns On Investment</h2>
+                  </div>
+                </div>
+                <div class="span4">
+                  <div class="cta-btn">
+                    <a href="#" class="btn btn-color btn-success">Start Funding <i class="icon-caret-right"></i></a>
+                  </div>
+                </div>
+
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section id="offer">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="row">
+              <div class="span12">
+                <div class="aligncenter">
+                  <h3>Most Recent Opportunities</h3>
+                  <blockquote>
+                    Don't work for money, make money work for you. - Robert Kiyosaki
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+
+              <div class="span3">
+                <h5>Do Not Miss The Moment</h5>
+                <p>
+                    On YouVest, opportunities come again and again,
+                    but they get sold out very quickly. Only those who
+                    jump in quickly get to enjoy the juice.
+                </p>
+                <p>
+                    Karen Lamb said; 
+                    <strong>
+                    a year from now you may wish you started today.
+                    </strong>
+                    But we say, a day from now, you may wish you started right now.
+                </p>
+              </div>
+
+              <div class="span3">
+                <div class="pricing-box-plain">
+                  <div class="heading">
+                    <h4>Grand Roaster</h4>
+                    <span>Status: Available</span>
+                  </div>
+                  <div class="desc">
+                    <ul>
+                      <li>6 Months</li>
+                      <li>ROI: 9%</li>
+                      <li>Unlimited disk spaces</li>
+                    </ul>
+                  </div>
+                  <div class="action">
+                    <a href="#" class="btn btn-inverse">Know More</a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="span3">
+                <div class="pricing-box-plain">
+                  <div class="heading">
+                    <h4>TransOcean</h4>
+                    <span>Status: Sold Out</span>
+                  </div>
+                  <div class="desc">
+                    <ul>
+                      <li>5 Months</li>
+                      <li>ROI: 10%</li>
+                      <li>Unlimited disk spaces</li>
+                    </ul>
+                  </div>
+                  <div class="action">
+                    <a href="#" class="btn btn-inverse">Know More</a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="span3">
+                <div class="pricing-box-plain">
+                  <div class="heading">
+                    <h4>Medium</h4>
+                    <span>$29 / Month</span>
+                  </div>
+                  <div class="desc">
+                    <ul>
+                      <li>For 10 Applications</li>
+                      <li>Free life time support</li>
+                      <li>Unlimited disk spaces</li>
+                    </ul>
+                  </div>
+                  <div class="action">
+                    <a href="#" class="btn btn-inverse">Know More</a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <!--
+    <section id="works">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <h3>Empowered By</h3>
+            <p>Your funds and data are protected by:</p>
+            <div class="row">
+
+              <div class="grid cs-style-3">
+                <div class="span3">
+                  <div class="item">
+                    <figure>
+                      <div><img src="img/dummies/works/1.jpg" alt=""></div>
+                      <figcaption>
+                        <h3>Portfolio name</h3>
+                        <p>
+                          <a href="img/dummies/works/big.png" data-pretty="prettyPhoto[gallery1]" title="Portfolio caption here"><i class="icon-zoom-in icon-circled icon-bglight icon-2x active"></i></a>
+                          <a href="#"><i class="icon-file icon-circled icon-bglight icon-2x active"></i></a>
+                        </p>
+                      </figcaption>
+                    </figure>
+                  </div>
+                </div>
+                <div class="span3">
+                  <div class="item">
+                    <figure>
+                      <div><img src="img/dummies/works/2.jpg" alt=""></div>
+                      <figcaption>
+                        <h3>Portfolio name</h3>
+                        <p>
+                          <a href="img/dummies/works/big.png" data-pretty="prettyPhoto[gallery1]" title="Portfolio caption here"><i class="icon-zoom-in icon-circled icon-bglight icon-2x active"></i></a>
+                          <a href="#"><i class="icon-file icon-circled icon-bglight icon-2x active"></i></a>
+                        </p>
+                      </figcaption>
+                    </figure>
+                  </div>
+                </div>
+                <div class="span3">
+                  <div class="item">
+                    <figure>
+                      <div><img src="img/dummies/works/3.jpg" alt=""></div>
+                      <figcaption>
+                        <h3>Portfolio name</h3>
+                        <p>
+                          <a href="img/dummies/works/big.png" data-pretty="prettyPhoto[gallery1]" title="Portfolio caption here"><i class="icon-zoom-in icon-circled icon-bglight icon-2x active"></i></a>
+                          <a href="#"><i class="icon-file icon-circled icon-bglight icon-2x active"></i></a>
+                        </p>
+                      </figcaption>
+                    </figure>
+                  </div>
+                </div>
+                <div class="span3">
+                  <div class="item">
+                    <figure>
+                      <div><img src="img/dummies/works/4.jpg" alt=""></div>
+                      <figcaption>
+                        <h3>Portfolio name</h3>
+                        <p>
+                          <a href="img/dummies/works/big.png" data-pretty="prettyPhoto[gallery1]" title="Portfolio caption here"><i class="icon-zoom-in icon-circled icon-bglight icon-2x active"></i></a>
+                          <a href="#"><i class="icon-file icon-circled icon-bglight icon-2x active"></i></a>
+                        </p>
+                      </figcaption>
+                    </figure>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    -->
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="span4">
+            <div class="widget">
+              <div class="footer_logo">
+                <h3><a href="index.html">YouVest</a></h3>
+              </div>
+              <address>
+							  <strong>Remember company Inc.</strong><br>
+  							Somestreet KW 101, Park Village W.01<br>
+  							Jakarta 13426 Indonesia
+  						</address>
+              <p>
+                <i class="icon-phone"></i> (123) 456-7890 - (123) 555-7891 <br>
+                <i class="icon-envelope-alt"></i> email@domainname.com
+              </p>
+            </div>
+          </div>
+          <div class="span4">
+            <div class="widget">
+              <h5 class="widgetheading">Browse pages</h5>
+              <ul class="link-list">
+                <li><a href="#">Our company</a></li>
+                <li><a href="#">Terms and conditions</a></li>
+                <li><a href="#">Privacy policy</a></li>
+                <li><a href="#">Press release</a></li>
+                <li><a href="#">What we have done</a></li>
+                <li><a href="#">Our support forum</a></li>
+              </ul>
+
+            </div>
+          </div>
+          <div class="span4">
+            <div class="widget">
+              <h5 class="widgetheading">From flickr</h5>
+              <div class="flickr_badge">
+                <script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=8&amp;display=random&amp;size=s&amp;layout=x&amp;source=user&amp;user=34178660@N03"></script>
+              </div>
+              <div class="clear"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="sub-footer">
+        <div class="container">
+          <div class="row">
+            <div class="span6">
+              <div class="copyright">
+                <p><span>&copy; You International. All right reserved</span></p>
+              </div>
+
+            </div>
+
+            <div class="span6">
+              <div class="credits">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <a href="#" class="scrollup"><i class="icon-angle-up icon-rounded icon-bglight icon-2x"></i></a>
+
+
+</body>
+
+</html>
