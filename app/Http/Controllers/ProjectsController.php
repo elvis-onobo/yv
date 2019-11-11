d88371778388b3c2108527195de12dfce84c5f44
Elvis Onobo <elvis.onobo@gmail.com>
2019-11-11 12:42:42 -0800

    modified create method

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Project;
 use Auth\User;
@@ -16,38 +17,46 @@ class ProjectsController extends Controller
         return view('admin.project');
     }
 
+    /**
+     * insert new project to db or update if exists
+     */
     public function create_projects(Request $request){
         $data = $request->validate([
-            'phone' => 'nullable|digits:11',
-            'dob' => 'nullable|date',
-            'gender' => ['nullable', Rule::in(['male','female'])],
-            'picture' => 'nullable'
+            'project_picture' => 'nullable',
+            'title' => 'required',
+            'returns' => 'required',
+            'duration' => 'required',
+            'location' => 'required',
+            'minimum' => 'required',
+            'risk' => 'required'
         ]);
 
         if($request->picture != null){
             // delete the previous picture and store new one
-            $pixToDelete = Profile::find(auth()->user()->id)->picture;
+            $pixToDelete = Project::find(auth()->user()->id)->project_picture;
             Storage::disk('public')->delete($pixToDelete);
             $request->file('picture')->store('projects', 'public');
         }
 
         $values = [
-            'phone' => $request->phone,
-            'dob' => $request->dob,
-            'address' => $request->address,
-            'nationality' => $request->nationality,
-            'gender' => $request->gender,
-            'picture' => $request->picture != null ? $request->file('picture')->store('pictures', 'public') : Profile::find(auth()->user()->id)->picture 
+            'created_by' => auth()->user()->id,
+            'project_picture' => $request->picture != null ? $request->file('picture')->store('pictures', 'public') : Project::find(auth()->user()->id)->project_picture ,
+            'title' => $request->title,
+            'returns' => $request->returns,
+            'duration' => $request->duration,
+            'location' => $request->location,
+            'minimum_investment' => $request->minimum,
+            'risk' => $request->risk
         ];
 
         // save the data and redirect accordingly
-        if(Profile::updateOrInsert(
-            ['user_id' => auth()->user()->id],
+        if(Project::updateOrInsert(
+            ['created_by' => auth()->user()->id],
             $values
         )){
-            return redirect('/home')->with('status', 'Profile updated!');
+            return redirect('/admin/home')->with('status', 'Project Published!');
         }else{
-            return redirect('/profile')->with('status','There is an error, please check and correct it');
+            return back()->with('status','There is an error, please verify');
         }                
     }
 }
