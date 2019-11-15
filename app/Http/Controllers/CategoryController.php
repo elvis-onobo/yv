38332bf754d6aea894db7c98b5a1f54d1c7341bf
Elvis Onobo <elvis.onobo@gmail.com>
2019-11-15 18:41:53 +0100

    created category methods

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function create(){
+        return view('admin.category')
+    }
+
+    public function store(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
 }
